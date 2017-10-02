982410cde32ea68172e23790de59b26f68bde284
RJ Hallsted <rhallste@student.42.us.org>
2017-10-02 11:54:17 -0700

    Brought over libft header

---
+++ libft.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/18 11:31:58 by rhallste          #+#    #+#             */
+/*   Updated: 2017/10/02 11:53:48 by rhallste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
+void				ft_bzero(void *s, size_t n);
+void				ft_free_2d_array(void ***array, size_t size);
+t_list				*ft_lst_swap(t_list **lst_start, int fi, int si);
+size_t				ft_lstlen(t_list *item);
+void				*ft_memalloc(size_t len);
+void				ft_putchar(char c);
+void				ft_putchar_fd(char c, int fd);
+void				ft_putstr(char const *s);
+void				ft_putstr_fd(char const *s, int fd);
+char				*ft_strcpy(char *dst, char const *src);
+char				*ft_strdup(const char *s1);
+char				*ft_strnew(size_t size);
+size_t				ft_strlen(char const *s);
+
+#endif
