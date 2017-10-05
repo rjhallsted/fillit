72af68725771f8169d7bc75dc672066841d7e9d1
RJ Hallsted <rhallste@student.42.us.org>
2017-10-05 11:39:52 -0700

    Renamed variables in check_input

--- check_input.c
+++ check_input.c
@@ -6,7 +6,7 @@
 /*   By: sjuery <sjuery@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/01 12:27:37 by sjuery            #+#    #+#             */
-/*   Updated: 2017/10/04 21:13:57 by sjuery           ###   ########.fr       */
+/*   Updated: 2017/10/05 11:36:01 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@
 unsigned int     check_input(char *tetriminos)
 {
     int i;
-    int c;
-    int ct;
-    int tc;
-    int cc;
+    int line_len;
+    int piece_line_count;
+    int pound_count;
+    int connection_count;
 
-    cc = 0;
-    ct = 0;
-    tc = 0;
+    connection_count = 0;
+    piece_line_count = 0;
+    pound_count = 0;
     i = 0;
-    c = 0;
+    line_len = 0;
     if (!tetriminos[i])
         return (0);
     while (tetriminos[i])
@@ -33,46 +33,46 @@ unsigned int     check_input(char *tetriminos)
         if (tetriminos[i] == '#')
         {
             if(tetriminos[i+1] == '#')
-                cc++;
+                connection_count++;
             if(tetriminos[i-1] == '#')
-                cc++;
+                connection_count++;
             if(tetriminos[i+5] == '#' &&
                 ((tetriminos[i+1] != '\n' && tetriminos[i+2] != '\n') ||
                 (tetriminos[i+2] != '\n' && tetriminos[i+3] != '\n') ||
                 (tetriminos[i+3] != '\n' && tetriminos[i+4] != '\n')))
-                cc++;
+                connection_count++;
             if(tetriminos[i-5] == '#' &&
                 ((tetriminos[i-1] != '\n' && tetriminos[i-2] != '\n') ||
                 (tetriminos[i-2] != '\n' && tetriminos[i-3] != '\n') ||
                 (tetriminos[i-3] != '\n' && tetriminos[i-4] != '\n')))
-                cc++;
-            tc++;
-            c++;
+                connection_count++;
+            pound_count++;
+            line_len++;
         }
         else if (tetriminos[i] == '.')
-            c++;
-        else if ((tetriminos[i] == '\n' || tetriminos[i] == '\0') && c == 4)
+            line_len++;
+        else if ((tetriminos[i] == '\n' || tetriminos[i] == '\0') && line_len == 4)
         {
-            c = 0;
-            ct++;
+            line_len = 0;
+            piece_line_count++;
         }
-        else if (tetriminos[i] == '\n' && c == 0 && tc == 4)
+        else if (tetriminos[i] == '\n' && line_len == 0 && pound_count == 4)
         {
-            if(!(cc == 6 || cc == 8))
+            if(!(connection_count == 6 || connection_count == 8))
                 return (0);
-            cc = 0;
-            ct = 0;
-            tc = 0;
+            connection_count = 0;
+            piece_line_count = 0;
+            pound_count = 0;
         }
         else
             return (0);
         i++;
-        if (tetriminos[i] == '\0' && ct != 4)
+        if (tetriminos[i] == '\0' && piece_line_count != 4)
             return (0);
     }
-    if(!(cc == 6 || cc == 8))
+    if(!(connection_count == 6 || connection_count == 8))
         return (0);
-    if(tc != 4)
+    if(pound_count != 4)
         return (0);
     return (1);
 }
